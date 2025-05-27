1664191e27e48b115b868f0d586b3dbde16e8db1
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-05-27 15:40:33 +0200

    Add ticket price calculation logic and user prompts

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("prova");
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, 
+// per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// Creare tre varibili per il numero di chilometri, l'età e il prezzo del biglietto.
+const numeroChilometri = Number(prompt("Inserisci il numero di Km:"));
+const age = Number(prompt("Inserisci l'età"));
+let ticketPrice;
+
+// Calcolare il prezzo totale moltiplicando il prezzo per il numero di chilometri.
+ticketPrice = numeroChilometri * 0.21;
+console.log("Prezzo totale:", ticketPrice)
+
+// Controllare se il passeggero è minorenne o over 65.
+if (age >= 18 && age <= 65) {
+    ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
+    console.log("Hai uno sconto del 20%! Prezzo totale:", ticketPrice);
+} else if (age > 65) {
+    ticketPrice = ticketPrice - ((ticketPrice * 40) / 100);
+    console.log("Hai uno sconto del 40%! Prezzo totale:", ticketPrice);
+}
